b024898bdb604cd9181b4813c75deddf38e6e8e0
beals <bealsben@gmail.com>
2025-02-09 18:33:09 -0800

    Patch to allow for Linux users to continue to install with Wine

--- Dotnet/Cef/Wine.cs
+++ Dotnet/Cef/Wine.cs
@@ -10,14 +10,7 @@ namespace VRCX
 
         public static bool GetIfWine()
         {
-            // wine_get_version should be guaranteed to exist exclusively in Wine envs,
-            // unlike some other suggestions like checking Wine registry keys 
-            try
-            {
-                wine_get_version();
-                return true;
-            }
-            catch { return false; }
+            return false;
         }
     }
 }
